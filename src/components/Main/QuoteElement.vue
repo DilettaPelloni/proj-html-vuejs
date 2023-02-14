93e773dde79e7d2f770e2db9b5ae488b593fbbd3
Diletta Pelloni <dilpel@live.it>
2023-02-14 17:18:40 +0100

    pulito quote element

--- src/components/Main/QuoteElement.vue
+++ src/components/Main/QuoteElement.vue
@@ -6,18 +6,20 @@
 
 <template>
     <section>
-        <div class="text-box">
-            <h2>
-                go ahead and
-                <br>
-                build your
-                <br>
-                own pizza
-                <br>
-                we won't
-                <br>
-                judge!
-            </h2>
+        <div class="container">
+            <div class="text-box">
+                <h2>
+                    go ahead and
+                    <br>
+                    build your
+                    <br>
+                    own pizza
+                    <br>
+                    we won't
+                    <br>
+                    judge!
+                </h2>
+            </div>
         </div>
     </section>
 </template>
@@ -25,15 +27,14 @@
 <style  lang="scss" scoped>
 section {
     padding-block: 5rem;
-    padding-left: 20%;
 
     background-image: url('../../assets/img/h3-background-img.jpg');
     background-size: cover;
     background-position: center;
 
     .text-box {
+        padding-left: 50px;
         color: white;
-        text-transform: uppercase;
         font-size: 2rem;
         line-height: 0.9;
     }//text-box
